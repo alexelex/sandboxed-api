338e3f722c636d298375b44802ad125bdc479652
Alexandra Latysheva <alexelexa@gmail.com>
2020-10-30 09:53:32 +0000

    change way to get path to test images

--- oss-internship-2020/libtiff/test/helper.cc
+++ oss-internship-2020/libtiff/test/helper.cc
@@ -14,23 +14,35 @@
 
 #include "helper.h"  // NOLINT(build/include)
 
-std::string GetImagesFolder() {
-  std::string cwd = sandbox2::file_util::fileops::GetCWD();
-  auto find = cwd.rfind("/build");
-  if (find == std::string::npos) {
-    LOG(ERROR) << "Something went wrong: CWD don't contain build dir. "
-               << "Please run tests from build dir, path might be incorrect\n";
+#include "../sandboxed.h"  // NOLINT(build/include)
+#include "sandboxed_api/sandbox2/util/fileops.h"
+#include "sandboxed_api/sandbox2/util/path.h"
+#include "sandboxed_api/sandbox2/util/runfiles.h"
 
-    return sandbox2::file::JoinPath(cwd, "test", "images");
+constexpr absl::string_view kWarning =
+    "Something went wrong: CWD don't contain expected dir. Please run build "
+    "project in the libtiff/build dir. To run example send project dir as a "
+    "parameter: ./sandboxed /absolute/path/to/project/dir .\n"
+    "Falling back to using current working directory as project dir.\n";
+
+std::string GetFilePath(absl::string_view filename) {
+  std::string exec_file_path = sandbox2::GetDataDependencyFilePath("");
+  std::string project_path = exec_file_path;
+
+  // exec_file_path is /path/to/project/build/exec_file_dir
+  // project_path is exec_file_path/../../
+  for (size_t i = 0; i != 2; ++i) {
+    auto find = project_path.rfind("/");
+    if (find == std::string::npos) {
+      LOG(ERROR) << kWarning;
+      return sandbox2::file::JoinPath(exec_file_path, "test", "images");
+    }
+    project_path = project_path.substr(0, find);
   }
 
-  return sandbox2::file::JoinPath(cwd.substr(0, find), "test", "images");
+  return sandbox2::file::JoinPath(project_path, "test", "images", filename);
 }
 
-std::string GetFilePath(const std::string& filename) {
-  static std::string* images_folder_path = nullptr;
-  if (!images_folder_path) {
-    images_folder_path = new std::string(GetImagesFolder());
-  }
-  return sandbox2::file::JoinPath(*images_folder_path, filename);
+std::string GetFilePath(absl::string_view dir, absl::string_view filename) {
+  return sandbox2::file::JoinPath(dir, "test", "images", filename);
 }
